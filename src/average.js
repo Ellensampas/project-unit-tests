81b28bf431f9daee49f83ab5dc96773715939ae5
ellensampaio <ellenssbarross@gmail.com>
2022-08-17 15:47:19 -0300

    conclui requisito 1

--- src/average.js
+++ src/average.js
@@ -11,6 +11,18 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
+const average = (array) => {
+  let soma = 0;
+  if (array.length <= 0) {
+    return undefined;
+  }
+  for (let i = 0; i < array.length; i += 1) {
+    soma += array[i];
+    if (typeof array[i] !== 'number') {
+      return undefined;
+    }
+  } 
+  return Math.round(soma / array.length);
+};
 
 module.exports = average;
